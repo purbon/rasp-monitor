a877a5684015919bc98a5171e21f272d83324bef
Pere Urbon <pere@confluent.io>
2021-11-19 18:40:08 +0100

    add reading and writing for temp and humidity

--- pm_reader.py
+++ pm_reader.py
@@ -4,6 +4,7 @@ import serial
 import time
 import os
 from Adafruit_IO import Client, Feed
+import Adafruit_DHT
 
 ser = serial.Serial('/dev/ttyUSB0')
 aio = Client(os.environ['ADAFRUIT_IO_USERNAME'], os.environ['ADAFRUIT_IO_KEY'])
@@ -15,6 +16,8 @@ aio = Client(os.environ['ADAFRUIT_IO_USERNAME'], os.environ['ADAFRUIT_IO_KEY'])
 
 room25feed = aio.feeds('roompmtwofive')
 room10feed = aio.feeds('roompmtwoten')
+roomTempFeed = aio.feeds('roomtemp')
+roomHumFeed = aio.feeds('roomhumidity')
 
 while True:
     data = []
@@ -23,9 +26,12 @@ while True:
         data.append(datum)
 
     pmt25 = int.from_bytes(b''.join(data[2:4]), byteorder='little') / 10
-    #print("pmt25", pmt25)
     aio.send_data(room25feed.key, pmt25)
     pmt10 = int.from_bytes(b''.join(data[4:6]), byteorder='little') / 10
-    #print("pmt10", pmt10)
     aio.send_data(room10feed.key, pmt10)
+
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+    aio.send_data(roomTempFeed.key, temperature)
+    aio.send_data(roomHumFeed.key, humidity)
+
     time.sleep(60)
