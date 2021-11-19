3875bc2dd768903bf88086266f9599e9107aa3f4
Pere Urbon <pere@confluent.io>
2021-11-19 13:13:09 +0100

    add connection to adafruit dashboard

--- pm_reader.py
+++ pm_reader.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
-import serial, time
+import serial
+import time
+import os
 from Adafruit_IO import Client
 
 ser = serial.Serial('/dev/ttyUSB0')
-
+aio = Client(os.environ['ADAFRUIT_IO_USERNAME'], os.environ['ADAFRUIT_IO_KEY'])
 while True:
     data = []
     for index in range(0, 10):
@@ -12,7 +14,9 @@ while True:
         data.append(datum)
 
     pmt25 = int.from_bytes(b''.join(data[2:4]), byteorder='little') / 10
-    print("pmt25", pmt25)
+    #print("pmt25", pmt25)
+    Client.send("roompmtwofive", pmt25)
     pmt10 = int.from_bytes(b''.join(data[4:6]), byteorder='little') / 10
-    print("pmt10", pmt10)
+    #print("pmt10", pmt10)
+    Client.send("roompmtwoten", pmt10)
     time.sleep(10)
