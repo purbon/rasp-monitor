fd8ec61cd9a3fb833fa648f821c75e4dfe13b555
Pere Urbon <pere@confluent.io>
2021-11-19 10:33:09 +0100

    pm reader first commit

---
+++ pm_reader.py
@@ -0,0 +1,16 @@
+import serial, time
+from Adafruit_IO import Client
+
+ser = serial.Serial('/dev/ttyUSB0')
+
+while True:
+    data = []
+    for index in range(0, 10):
+        datum = ser.read()
+        data.append(datum)
+
+    pmt25 = int.from_bytes(b''.join(data[2:4]), byteorder='little') / 10
+    print("pmt25", pmt25)
+    pmt10 = int.from_bytes(b''.join(data[4:6]), byteorder='little') / 10
+    print("pmt10", pmt10)
+    time.sleep(10)
