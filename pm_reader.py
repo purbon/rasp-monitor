3d40480391fe72ce1eb89f87b477f1456dd35128
Pere Urbon <pere@confluent.io>
2021-11-19 13:19:54 +0100

    update pm reader ada api usage

--- pm_reader.py
+++ pm_reader.py
@@ -7,6 +7,10 @@ from Adafruit_IO import Client
 
 ser = serial.Serial('/dev/ttyUSB0')
 aio = Client(os.environ['ADAFRUIT_IO_USERNAME'], os.environ['ADAFRUIT_IO_KEY'])
+
+room25feed = aio.feeds('roompmtwofive')
+room10feed = aio.feeds('roompmtwoten')
+
 while True:
     data = []
     for index in range(0, 10):
@@ -15,8 +19,8 @@ while True:
 
     pmt25 = int.from_bytes(b''.join(data[2:4]), byteorder='little') / 10
     #print("pmt25", pmt25)
-    aio.send("roompmtwofive", pmt25)
+    aio.send_data(room25feed.key, pmt25)
     pmt10 = int.from_bytes(b''.join(data[4:6]), byteorder='little') / 10
     #print("pmt10", pmt10)
-    aio.send("roompmtwoten", pmt10)
+    aio.send_data(room10feed.key, pmt10)
     time.sleep(10)
