5c9054d13d700be4d2bd1cb5a5fd2e52690c9c45
Pere Urbon <pere@confluent.io>
2021-11-20 21:16:43 +0100

    print result to stdout for experiment

--- pm_reader.py
+++ pm_reader.py
@@ -9,30 +9,47 @@ import Adafruit_DHT
 ser = serial.Serial('/dev/ttyUSB0')
 aio = Client(os.environ['ADAFRUIT_IO_USERNAME'], os.environ['ADAFRUIT_IO_KEY'])
 
-#feed1 = Feed(name='roompmtwofive')
-#result1 = aio.create_feed(feed1)
-#feed2 = Feed(name='roompmtwoten')
-#result2 = aio.create_feed(feed2)
+def send(aio, pmt25, pmt10, temp, humidity):
+    print("sending data")
 
-room25feed = aio.feeds('roompmtwofive')
-room10feed = aio.feeds('roompmtwoten')
-roomTempFeed = aio.feeds('roomtemp')
-roomHumFeed = aio.feeds('roomhumidity')
+    room25feed = aio.feeds('roompmtwofive')
+    room10feed = aio.feeds('roompmtwoten')
+    roomTempFeed = aio.feeds('roomtemp')
+    roomHumFeed = aio.feeds('roomhumidity')
 
-while True:
-    data = []
-    for index in range(0, 10):
-        datum = ser.read()
-        data.append(datum)
-
-    pmt25 = int.from_bytes(b''.join(data[2:4]), byteorder='little') / 10
     aio.send_data(room25feed.key, pmt25)
-    pmt10 = int.from_bytes(b''.join(data[4:6]), byteorder='little') / 10
     aio.send_data(room10feed.key, pmt10)
-
-    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
-    #print("h", humidity, "t", temperature)
     aio.send_data(roomTempFeed.key, temperature)
     aio.send_data(roomHumFeed.key, humidity)
 
-    time.sleep(60)
+def save(file, pmt25, pmt10, temp, humidity):
+    print("saving data in file")
+
+def echo(pmt25, pmt10, temp, humidity):
+    print(f'{pmt25:.2f}, {pmt10:.2f}, {temp:.2f}, {humidity:.2f}')
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        mode = sys.argv[1] # First param, would be modus in lower case
+        store = sys.argv[2] # Store mode (send, for ada IO, store for file)
+    else:
+        mode = "all"
+
+    while True:
+        data = []
+        for index in range(0, 10):
+            datum = ser.read()
+            data.append(datum)
+
+        pmt25 = int.from_bytes(b''.join(data[2:4]), byteorder='little') / 10
+        pmt10 = int.from_bytes(b''.join(data[4:6]), byteorder='little') / 10
+
+        humidity = None
+        temperature = None
+
+        if mode == "all":
+            humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+
+        echo(pmt25, pmt10, temp, humidity)
+
+        time.sleep(60)
